0dd19b95e5ec91d04a23c54c3347bfc1d87bb174
Taylor Cuevas <Taylor.cuevas@fmr.com>
2022-06-22 11:16:43 -0600

    version 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,108 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import { SteckerService } from "./stecker.service";
+import { LogicService } from "./logic.service";
+import {FormControl} from "@angular/forms";
+import {ActivatedRoute, ActivatedRouteSnapshot, Params, Router} from "@angular/router";
+import {RotorComponent} from "./rotor/rotor.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+  @ViewChildren(RotorComponent) rotors!: QueryList<RotorComponent>;
+
+  radio = new FormControl();
+  inputControl = new FormControl();
+  outputControl = new FormControl();
+
   title = 'enigma';
+  rotorSelection = ['I','II','III','IV','V','VI','VII','VIII'];
+  rotorSettings: string[] =['','','','','','','',''];
+  selectedChars: string[] = ['','','','','','','',''];
+  rotation: number[] = [0,0,0,0,0,0,0,0];
+  pair: string[] = this.steckerService.steckeredPair;
+  input: string[] = [];
+  output: string[] = [];
+  encode: boolean = true;
+  debug: boolean = false;
+  selectedCharsString: string = '';
+  rotationsTemp: string = '';
+  querySteckersTemp: string = '';
+  querySteckers: string[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public steckerService: SteckerService,
+    public logicService: LogicService) {
+  }
+
+  ngAfterContentChecked() {
+    this.route.queryParams.subscribe(params => {
+      this.rotationsTemp = params['rotation'];
+      this.querySteckersTemp = params['steckers'];
+    })
+    if (this.rotationsTemp) {
+      this.querySteckers = this.querySteckersTemp.split(',');
+      this.rotation = this.rotationsTemp.split(',').map(x => parseInt(x));
+    }
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.rotors.forEach(rotor => rotor.wiggle());
+      for (let i = 0; i < this.querySteckers.length; i++) {
+        this.steckerService.steckerPair(this.querySteckers[i], this.querySteckers[i + 1]);
+        i++
+      }
+    }, 50)
+
+  }
+
+  processInput() {
+    if (this.encode) {
+      this.outputControl.setValue(this.logicService.encodeString(this.inputControl.value))
+    } else {
+      this.outputControl.setValue(this.logicService.decodeString(this.inputControl.value))
+    }
+  }
+
+  updateRotation(val: number, index: number) {
+    this.rotation[index] = val;
+    this.router.navigate(
+      [],
+      {
+        queryParams: {
+          rotation: this.rotation.toString()
+        },
+        queryParamsHandling: 'merge'
+      }
+    );
+  }
+
+  updateSelectedChars(char: string, index: number) {
+    this.selectedChars[index] = char;
+  }
+
+  updateRotorSettings(rotor: string, index: number) {
+    this.rotorSettings[index] = rotor;
+    this.logicService.rotors[index] = rotor;
+  }
+
+  changeMethod() {
+    switch (this.radio.value) {
+      case 'Encode':
+        this.encode = true;
+        break;
+      case 'Decode':
+        this.encode = false;
+        break;
+    }
+  }
+
+  changeDebug() {
+    this.debug = !this.debug;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RotorComponent } from './rotor/rotor.component';
+import { SwitchBoardComponent } from './switch-board/switch-board.component';
+import { OutputKeyboardComponent } from './output-keyboard/output-keyboard.component';
+import { BoldTextPipe } from './bold-text.pipe';
+import { SwitchBoardKeyComponent } from './switch-board/switch-board-key/switch-board-key.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule} from "@angular/router";
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RotorComponent,
+    SwitchBoardComponent,
+    OutputKeyboardComponent,
+    BoldTextPipe,
+    SwitchBoardKeyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
+    RouterModule.forRoot([]),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
